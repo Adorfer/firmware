a71e4c8a14e556791b1cf7062dd4c56e7372a57c
Adorfer <adorferen@gmail.com>
2025-01-21 19:19:58 +0100

    removing legacy configs, moving 4_32-Routers to SACKGASSE

--- build.sh
+++ build.sh
@@ -88,7 +88,7 @@ GITRESET=false
 
 #SBRANCH="$(date +%Y%m%d%H%M)"
 SBRANCH="$(date +%y%m%d%H)$(cat $1|grep -v ^#|head -1|cut -c1-3)"  
-SBRANCH="24111213sta"
+SBRANCH="24111214sackgasse"
 #SBRANCH="24041622"
 
 SITECODE_BEFORE="coldstart"
@@ -338,7 +338,7 @@ build_images_for_site ()
   local MAKE_J_VAL
   MAKE_J_VAL="$(( $(getconf _NPROCESSORS_ONLN) * 2 ))"
 # only 1 cpu core to use
-#  MAKE_J_VAL=1
+  MAKE_J_VAL=1
   for (( target_index=0; target_index < ${#TARGETS[@]}; target_index += 1 )); do
     TARGET="${TARGETS[target_index]}"
     echo "GLUONDEVICEs $GLUONDEVICES"
@@ -381,33 +381,33 @@ build_all_images ()
 {
   local -a TARGETS=("$@")
   if (( ${#TARGETS[@]} == 0 )); then
-#    TARGETS+=( ar71xx-tiny )
-#    TARGETS+=( ar71xx-generic )
-#    TARGETS+=( ar71xx-nand )
-#    TARGETS+=( ar71xx-mikrotik )
-#    TARGETS+=( ath79-generic )
-##########   TARGETS+=( brcm2708-bcm2708 )
-##########   TARGETS+=( brcm2708-bcm2709 )
-##########   TARGETS+=( brcm2708-bcm2710 )
-#    TARGETS+=( mvebu-cortexa9 )
-#    TARGETS+=( ipq40xx-generic )
-#    TARGETS+=( ipq806x-generic )
-#    TARGETS+=( lantiq-xrx200 )
-#    TARGETS+=( lantiq-xway )
-#    TARGETS+=( mpc85xx-generic )
-#    TARGETS+=( mpc85xx-p1020 )
-#    TARGETS+=( ramips-mt7620 )
-#    TARGETS+=( ramips-mt76x8 )
-#    TARGETS+=( ramips-rt305x )
-#    TARGETS+=( sunxi-cortexa7 )
-#    TARGETS+=( x86-generic )
-##    TARGETS+=( x86-geode )
-#    TARGETS+=( x86-64 )
-######    TARGETS+=( x86-legacy )
-    TARGETS+=( ramips-mt7621 )
-#    ADD_MI4G=false     
-    ADD_MI4G=true     
-##    TARGETS+=( rockchip-armv8 )
+    TARGETS+=( ar71xx-tiny )
+    TARGETS+=( ar71xx-generic )
+##    TARGETS+=( ar71xx-nand )
+##    TARGETS+=( ar71xx-mikrotik )
+##    TARGETS+=( ath79-generic )
+###########   TARGETS+=( brcm2708-bcm2708 )
+###########   TARGETS+=( brcm2708-bcm2709 )
+###########   TARGETS+=( brcm2708-bcm2710 )
+##    TARGETS+=( mvebu-cortexa9 )
+##    TARGETS+=( ipq40xx-generic )
+##    TARGETS+=( ipq806x-generic )
+##    TARGETS+=( lantiq-xrx200 )
+##    TARGETS+=( lantiq-xway )
+##    TARGETS+=( mpc85xx-generic )
+##    TARGETS+=( mpc85xx-p1020 )
+##    TARGETS+=( ramips-mt7620 )
+##    TARGETS+=( ramips-mt76x8 )
+##    TARGETS+=( ramips-rt305x )
+##    TARGETS+=( sunxi-cortexa7 )
+##    TARGETS+=( x86-generic )
+###    TARGETS+=( x86-geode )
+##    TARGETS+=( x86-64 )
+#######    TARGETS+=( x86-legacy )
+##    TARGETS+=( ramips-mt7621 )
+##    ADD_MI4G=false     
+##    ADD_MI4G=true     
+###    TARGETS+=( rockchip-armv8 )
    fi
 
   pushd "$GLUON_DIR" >/dev/null
