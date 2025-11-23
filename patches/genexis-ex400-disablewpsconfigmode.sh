95b365dd21ad999f3c07c8e2300ac6d7fdf1cd06
Adorfer <adorferen@gmail.com>
2025-11-23 05:32:16 +0100

    removing genexis-ex400-disablewpsconfigmode as it is gluon release now

--- patches/genexis-ex400-disablewpsconfigmode.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo $PWD 
-
-  echo "Applying patch for NoWPS-Touchbutton for ramips-mt7621/genexis-ex400"
-  patchfile="../patches/genexis-ex400-disablewpsconfigmode.patch"
-  if ! patch -R -p1 -s -f --ignore-whitespace --dry-run <$patchfile &>/dev/null; then
-    patch -p1 --ignore-whitespace <$patchfile
-   fi
-  echo -n "patch in target?: ";grep -A1 'genexis-pulse-ex400' package/gluon-setup-mode/files/etc/hotplug.d/button/50-gluon-setup-mode
