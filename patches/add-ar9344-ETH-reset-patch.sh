cbd757749a6d4bfd02ab95d7104c90badd013c06
Adorfer <adorferen@gmail.com>
2025-11-23 05:29:47 +0100

    add-ar9344-ETH-reset-patch made it into gluon release now, no need to patch ourselves

--- patches/add-ar9344-ETH-reset-patch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo $PWD 
-
-  echo "Applying patches for TL-WR3600/WR4300 reboot patches on openwrt"
-  pushd openwrt
-  [ -f target/linux/ath79/patches-5.15/101-reset-ath79-reset-ETH-switch-for-AR9344.patch ] && echo "target/linux/ath79/patches-5.15/101-reset-ath79-reset-ETH-switch-for-AR9344.patch exists, skipping copy." || cp ../../patches/101-reset-ath79-reset-ETH-switch-for-AR9344.patch target/linux/ath79/patches-5.15/101-reset-ath79-reset-ETH-switch-for-AR9344.patch
-  popd
