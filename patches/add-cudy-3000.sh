060231cdc165ad5c933c0f57d08eaee4c17396f2
Adorfer <adorferen@gmail.com>
2025-11-23 02:48:17 +0100

    patch for cudy series 3000 filogic routers

--- patches/add-cudy-3000.sh
+++ patches/add-cudy-3000.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 echo $PWD 
 
-  echo "Applying patches for mediatek-filogic/cudy-wr3000e on gluon"
-  patchfile="../patches/add-cudy-wr3000e-gluon.patch"
+  echo "Applying patches for mediatek-filogic/cudy-3000 series on gluon"
+  patchfile="../patches/add-cudy-3000-gluon.patch"
   if ! patch -R -p1 -s -f --ignore-whitespace --dry-run <$patchfile &>/dev/null; then
     patch -p1 -f --ignore-whitespace <$patchfile
    fi
   grep 'wr3000e' targets/mediatek-filogic
 
   cd openwrt 
-  patchfile="../../patches/add-cudy-wr3000e-openwrt.patch"
+  patchfile="../../patches/add-cudy-3000-openwrt.patch"
   if ! patch -R -p1 -s -f --ignore-whitespace --dry-run <$patchfile &>/dev/null; then
     patch -p1 -f --ignore-whitespace <$patchfile
    fi
   grep  'wr3000e' target/linux/mediatek/image/filogic.mk
-
 
 #[ -f target/linux/mediatek/dts/mt7981b-cudy-wr3000e-v1.dts ] && echo "target/linux/mediatek/dts/mt7981b-cudy-wr3000e-v1.dts exists, skipping copy." || cp ../../patches/XIAOMI-MIR4A-GIGABIT.dts target/linux/mediatek/dts/mt7981b-cudy-wr3000e-v1.dts
 #[ -f target/linux/mediatek/dts/mt7981b-cudy-wr3000-nand.dtsi ] && echo "target/linux/mediatek/dts/mt7981b-cudy-wr3000-nand.dtsi exists, skipping copy." || cp ../../patches/XIAOMI-MIR4A-GIGABIT.dts target/linux/mediatek/dts/mt7981b-cudy-wr3000-nand.dtsi
